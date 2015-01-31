7e5791a09afbebccb7475fba2fddb441f8b0b33b
jimtoepel <jim.toepel@gmail.com>
2015-01-31 14:42:42 -0800

    added finish line

---
+++ Assets/Scripts/FinishLineRenderer.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+
+var post_Left: Transform;
+var post_Right: Transform;
+
+var lineColor : Color = Color.green;
+
+
+function Start () {
+
+	var lineRenderer : LineRenderer = gameObject.AddComponent(LineRenderer);
+	lineRenderer.SetPosition(0, post_Left.position);
+	lineRenderer.SetPosition(1, post_Right.position);
+	lineRenderer.material = new Material (Shader.Find("Particles/Additive"));
+	lineRenderer.SetColors(lineColor, lineColor);
+
+}
