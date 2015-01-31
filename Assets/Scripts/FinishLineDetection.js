92bf3e05a3a9da0c44c672990127d570f926261d
jimtoepel <jim.toepel@gmail.com>
2015-01-31 14:47:01 -0800

    Finish line logic in.

---
+++ Assets/Scripts/FinishLineDetection.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+
+function OnTriggerEnter(other : Collider) {
+
+	if (other.gameObject.tag == "Player")
+	{
+		Debug.Log("You made it!!!");
+	}
+
+}
+
+@script RequireComponent(Collider)
