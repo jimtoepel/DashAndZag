98461c92875715a181b8a0d1109f6cd856260e74
jimtoepel <jim.toepel@gmail.com>
2015-01-31 17:20:47 -0800

    Crappy GUI up and running

---
+++ Assets/Scripts/GameController.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+
+static var gameRunning : boolean = true;
+
+var gameTimeAllowed : float = 20.0;
+
+private var gameMessageLabel = "";
+private var gameMessageDisplay : Rect;
+private var timedOut : boolean = false;
+private var gameTimeRemaining : float = gameTimeAllowed;
+
+
+
+function Awake () {
+	gameMessageDisplay = Rect (10,10, Screen.width - 20, 40);
+
+}
+
+
+function OnGUI () {
+	GUI.color = Color.yellow;
+	GUI.backgroundColor = Color.black;
+	
+	var text : String = "";
+	if (timedOut) {
+		gameMessageLabel = "Time's up!!";
+	} else {
+		text = String.Format( "{0:00}:{1:00}", parseInt(gameTimeRemaining /60.0), parseInt(gameTimeRemaining % 60.0));
+		gameMessageLabel = "Time: Left: " + text;
+	}
+	GUI.Box(gameMessageDisplay, gameMessageLabel);
+}
+
+
+function Update () {
+
+	if (!gameRunning)
+		return;
+		
+		// Keep Track of time and display a countdown
+	gameTimeRemaining -= Time.deltaTime;
+	if (gameTimeRemaining <= 0) {
+		timedOut = true;
+		gameRunning = false;
+	}
+	
+}
---
+++ Assets/Scripts/ObstacleLauncher.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+var projectile : Rigidbody;
+var speed = 5;
+var maxObstacles = 2;
+var launchInterval : float = 5.0;
+var target : Transform;
+
+private var nextLaunch : float = 0.0;
+private var numObstaclesLaunched = 0;
+
+
+function Start () {
+	if (target == null) {
+	// find player transform
+	target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+}
+
+function Update () {
+	if ((numObstaclesLaunched < maxObstacles) && (Time.time > nextLaunch)) {
+	// set up the next launch time
+	nextLaunch = Time.time + launchInterval;
+	
+	// set the direciton
+	var hit : RaycastHit;
+	var ray : Ray;
+	var hitDistance : float;
+	
+	// Instantiate the projectile
+	var instantiatedProjectile : Rigidbody = Instantiate(projectile, transform.position, transform.rotation);
+	
+	// Try and put it in front of the player
+	
+	instantiatedProjectile.velocity = target.TransformDirection(-Vector3.forward*speed);
+	
+	// keep track of how many launched
+	numObstaclesLaunched ++;
+	
+	}
+
+}
